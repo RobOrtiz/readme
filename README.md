6947d0983477a6cebf912c9c1516720097f6f744
RobOrtiz <mrrobortiz@gmail.com>
2020-12-06 20:40:30 -0800

    finished index js file and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# readme
+# README Project
+    [licenses](undefined)
+        ## Generate a README file
+
+        ## Table of Content
+        - [description](#description)
+        - [installation](#installation)
+        - [usage](#usage)
+        - [licenses](#licenses)
+        - [contribution](#contribution)
+        - [test](#test)
+        - [questions](#questions)
+        - [username](#username)
+
+        This project allows people to input their information and it generates a README file with headers and all.
+        ## *description*
+
+        undefined
+        ## *username*
+
+        Answer all prompted questions
+        ## *installation*
+
+        Class assignment
+        ## *usage*
+
+        MIT
+        ## *licenses*
+
+        Robert Ortiz
+        ## *contribution*
+
+        N/A
+        ## *test*
+
+        undefined
+        ## *email*
+
+        undefined
+        ## *screenshot*
+        
--- index.js
+++ index.js
@@ -1,72 +1,130 @@
-const fs = require("fs");
-const inquirer = require("inquirer");
-const Title = require("./title");
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-const title = new Title();
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 const promptUser = () => 
-    inquirer.prompt([
-        {   
-            message: "What is the name of your project?",
-            name: "title",
-            type: "input",
-        },
-        {   
-            message: "Provide a table of content",
-            name: "table of content",
-            type: "input",
-        },
-        {  
-            message: "Please provide a description of the project",
-            name: "description",
-            type: "input",
-        },
-        {   
-            message: "What is the name of the user?",
-            name: "userName",
-            type: "input",
-        },
-        {   
-            message: "What is the installation process?",
-            name: "installation",
-            type: "input",
-        },
-        {   
-            message: "Purpose of the project?",
-            name: "usage",
-            type: "input",
-        },
-        {   
-            message: "What licenses are required with this project?",
-            name: "licenses",
-            type: "input",
-        },
-        {   
-            message: "Who contributed to this project?",
-            name: "contribution",
-            type: "input",
-        },
-        {   
-            message: "How did you test your project?",
-            name: "test",
-            type: "input",
-        },
-        {   
-            message: "What is the user github email address?",
-            name: "GitHub user email",
-            type: "input",
-        },
-        {   
-            message: "Please provide a profile picture",
-            name: "GitHub profile picture",
-            type: "input",
-        },
-    ]);
+inquirer.prompt([
+    {   
+        message: "What is the name of your project?",
+        name: "title",
+        type: "input",
+    },
+    {   
+        message: "Provide a quick summary",
+        name: "summary",
+        type: "input",
+    },
+    {   
+        message: "Provide a table of content",
+        name: "table of content",
+        type: "input",
+    },
+    {  
+        message: "Please provide a description of the project",
+        name: "description",
+        type: "input",
+    },
+    {   
+        message: "What is your github username?",
+        name: "userName",
+        type: "input",
+    },
+    {   
+        message: "What is the installation process?",
+        name: "installation",
+        type: "input",
+    },
+    {   
+        message: "Purpose of the project?",
+        name: "usage",
+        type: "input",
+    },
+    {   
+        message: "Choose a license",
+        name: "licenses",
+        type: "checkbox",
+        choices: [
+            "MIT",
+            "Apache",
+            "ISC",
+            "GNU"]
+    },
+    {   
+        message: "Who contributed to this project?",
+        name: "contribution",
+        type: "input",
+    },
+    {   
+        message: "How did you test your project?",
+        name: "test",
+        type: "input",
+    },
+    {   
+        message: "What is the user github email address?",
+        name: "GitHub user email",
+        type: "input",
+    },
+    {   
+        message: "Please provide a screenshot image file path",
+        name: "Screenshot",
+        type: "input",
+    },
+])
+
+    const generateREADME = (answers) => 
+    `# ${answers.title}
+    [licenses](${answers.badge})
+        ## ${answers.summary}
+
+        ## Table of Content
+        - [description](#description)
+        - [installation](#installation)
+        - [usage](#usage)
+        - [licenses](#licenses)
+        - [contribution](#contribution)
+        - [test](#test)
+        - [questions](#questions)
+        - [username](#username)
+
+        ${answers.description}
+        ## *description*
+
+        ${answers.username}
+        ## *username*
+
+        ${answers.installation}
+        ## *installation*
+
+        ${answers.usage}
+        ## *usage*
+
+        ${answers.licenses}
+        ## *licenses*
+
+        ${answers.contribution}
+        ## *contribution*
+
+        ${answers.test}
+        ## *test*
+
+        ${answers.email}
+        ## *email*
+
+        ${answers.screenshot}
+        ## *screenshot*
+        `
     
-    promptUser()
-    .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
-    .then(() => console.log('Successfully wrote to README.md'))
-    .catch((err) => console.error(err));
-
-
+     promptUser()
+        .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+        .then(() => console.log("Great Job! You successfully wrote to README.md"))
+        .catch((err) => console.error(err));
+// fs.writeFile('README.md', generateREADME, err => {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("Great Job! You successfully wrote to README.md");
+//     }
+// };
